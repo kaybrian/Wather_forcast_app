119a560cede81c00a385ecf2792dde1311aebbf3
kayongo <kayonbrian@gmail.com>
2022-10-09 18:27:30 +0200

    able to get a persons ip address

--- main/views.py
+++ main/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.views import View
 
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 # Create your views here.
+class HomePageView(View):
+    def get(self,request):
+        ip = get_client_ip(request)
+        response = DbIpCity.get(str(ip), api_key='free')
+        print(response.city)
+        context ={}
+        return render(request, 'index.html', context)
